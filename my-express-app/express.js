41f08bfe96616421d0c4c00ae015a6d28408666f
Seun <seunaladedoye@gmail.com>
2024-06-15 00:43:14 +0100

    Implementing Rest Apis

--- my-express-app/express.js
+++ my-express-app/express.js
@@ -8,6 +8,10 @@ app.get('/', (req,res)=>{
 res.sendFile(__dirname +"/public/index.html");
 });
 
+app.get('/contact',(req,res)=>{
+    res.sendFile(__dirname + "/public/contact.html")
+});
+
 app.listen(port,()=>{
 console.log('Server is running on Port 4000')
 });
