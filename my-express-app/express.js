4d9ac9d086c947e45999745e2d1301e25c38b035
Seun <seunaladedoye@gmail.com>
2024-06-15 00:53:49 +0100

    Implementing Rest Api 2

--- my-express-app/express.js
+++ my-express-app/express.js
@@ -3,15 +3,16 @@ const app=express();
 const port=4000;
 // Static Files
 app.use(express.static(__dirname + "/public"));
-
+//HTTP METHODS
 app.get('/', (req,res)=>{
 res.sendFile(__dirname +"/public/index.html");
 });
-
 app.get('/contact',(req,res)=>{
     res.sendFile(__dirname + "/public/contact.html")
 });
-
+app.get('/appointment',(req,res)=>{
+    res.sendFile(__dirname + "/public/appointment.html")
+});
 app.listen(port,()=>{
 console.log('Server is running on Port 4000')
 });
