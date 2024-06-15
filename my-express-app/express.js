8b5876a5280d048f059bf35d0175d495b8ffa13a
Seun <seunaladedoye@gmail.com>
2024-06-15 19:55:26 +0100

    User Authentication In Progress

--- my-express-app/express.js
+++ my-express-app/express.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const app=express();
 const port=4000;
+const bodyParser=require('body-parser');
 // Static Files
 app.use(express.static(__dirname + "/public"));
+//Body Parser
+app.use(bodyParser.urlencoded({extended:false}));
 //HTTP METHODS
 app.get('/', (req,res)=>{
 res.sendFile(__dirname +"/public/index.html");
@@ -16,6 +19,10 @@ app.get('/appointment',(req,res)=>{
 app.get('/admin',(req,res)=>{
     res.sendFile (__dirname + "/public/login.html")
 });
+app.post('/success',(req,res)=>{
+    console.log(req.body)
+    res.sendFile(__dirname + '/public/success.html')
+})
 //Listener
 app.listen(port,()=>{
 console.log('Server is running on Port 4000')
